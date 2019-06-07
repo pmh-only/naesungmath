9096b5d7bb36eca8e0a57024cffb0f5795c0f979
Naesung <42634731+Naesung@users.noreply.github.com>
2019-06-07 23:46:55 +0900

    Update math.js

--- math.js
+++ math.js
@@ -1,36 +1,37 @@
+//더하기//
 exports.plus = function (a, b) {
     return a + b;
  };
- 
+//빼기// 
  exports.minus = function (a, b) {
      return a - b;
  };
- 
+ //곱하기//
  exports.multiply = function (a, b) {
      return a * b;
  };
- 
+ //나누기//
  exports.divide = function (a, b) {
      return a / b;
  };
- 
+ //a의 b제곱//
  exports.caret = function (a, b) {
      return a ^ b;
  };
- 
+ //좌표//
  exports.pow = function(a, b){
   return Math.pow(a, b);
  };
- 
+ //제곱//
  exports.sqrt = function(a){
  return Math.sqrt(a);	
  };
- 
+ //랜덤//
  exports.random = function(a, b){
  var ranNum = Math.floor((Math.random() * b) + a); 
    return ranNum;
  };
- 
+ //소수//
  exports.primenumber = function(a, b) {
      let results = [];
      for (let i = a; i <= b; i++) {
@@ -48,44 +49,44 @@ exports.plus = function (a, b) {
  
      return results;
  };
- 
+ //호도법//
  exports.pi2 = function(a) {
      return 2 * Math.PI * a;
  };
- 
+ //파이//
  exports.pi= Math.PI;
  
  exports.round = function(a) {
     return Math.round(a);
  };
-
+//정삼각형의 넓이//
  exports.equilateraltrianglearea = function(a)
     return (3^0.5/4*a*a);
  };
-
+//정삼각형의 높이//
  exports.equilateraltriangleheight = function(a)
     return (3^0.5/2*a);
  };
-
+//직각삼각형의 넓이//
  exports.righttriangle = function(a, b)
     return (0.5*a*b);
  };
-
+//피타고라스 정리//
  exports.pytagoras = function(a, b)
     return (a*a+b*b)
  };
-
+//이등변삼각형의 넓이//
  exports.quadrilateraltriangle = function (a, b)
     return (a/4*(4*b*b-a*a)^0.5)
  };
-
+//헤론의 공식//
  exports.trianglearea = function(a, b, c)
     return ((((a+b+c)/2)*(((a+b+c)/2)-a)*(((a+b+c)/2)-b)*(((a+b+c)/2)-c))^0.5);
  };
-
+//삼각형의 넓이//
  exports.trianglewithheight = function (a, b, h)
  };
-
+//각의 삼각형의 넓이//
  exports.trianglewithangle = function (a, b, t)
     return (0.5*a*b*(sin(t)));
 };
