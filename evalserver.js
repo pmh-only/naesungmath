fe111754e95c780a812fa509d0d230db5de8f25c
129.05 <minseo0388@outlook.com>
2020-03-31 15:24:42 +0900

    Eslint

--- evalserver.js
+++ evalserver.js
@@ -1,10 +1,10 @@
-const nmath = require("./math.js");
-const assert = require('assert');
-const repl = require('repl');
+const nmath = require('./math.js')
+const assert = require('assert')
+const repl = require('repl')
 
-const help = "nmath repl 서버";
+const help = 'naesungmath repl 서버'
 
-const r = repl.start("> ");
-r.context.help = help;
-r.context.nmath = nmath;
-r.context.assert = assert;
+const r = repl.start('> ')
+r.context.help = help
+r.context.nmath = nmath
+r.context.assert = assert
