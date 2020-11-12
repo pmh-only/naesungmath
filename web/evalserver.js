4377fe76ca63eabf0df9d32cf402b8c25a45381a
Donghan <minseo0388@outlook.com>
2020-11-12 11:18:12 +0900

    File Sorted

---
+++ web/evalserver.js
@@ -0,0 +1,12 @@
+/*
+const nmath = require('./src')
+const assert = require('assert')
+const repl = require('repl')
+
+const help = 'naesungmath repl 서버'
+
+const r = repl.start('> ')
+r.context.help = help
+r.context.nmath = nmath
+r.context.assert = assert
+*/
